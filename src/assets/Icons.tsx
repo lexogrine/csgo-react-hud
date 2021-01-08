7dcfa37ed3819dc3fb8deabe8415c0c0caa029bb
Hubert Walczak <hubertwalczak8@gmail.com>
2021-01-08 21:29:53 +0100

    Move to correct directory

--- src/assets/Icons.tsx
+++ src/assets/Icons.tsx
@@ -1,25 +1,25 @@
-import { ReactComponent as ArmorFull } from './../assets/images/svg/icon_armor_full_default.svg';
-import { ReactComponent as ArmorHalf } from './../assets/images/svg/icon_armor_half_default.svg';
-import { ReactComponent as ArmorHalfHelmet } from './../assets/images/svg/icon_armor_half_helmet_default.svg';
-import { ReactComponent as ArmorHelmet } from './../assets/images/svg/icon_armor_helmet_default.svg';
-import { ReactComponent as ArmorNone } from './../assets/images/svg/icon_armor_none_default.svg';
-import { ReactComponent as Blind } from './../assets/images/svg/icon_blind.svg';
-import { ReactComponent as Bomb } from './../assets/images/svg/icon_bomb_default.svg';
-import { ReactComponent as BombExplosion }  from './../assets/images/svg/icon_bomb_explosion_default.svg';
-import { ReactComponent as Bullets }  from './../assets/images/svg/icon_bullets_default.svg';
-import { ReactComponent as Burning }  from './../assets/images/svg/icon_burning.svg';
-import { ReactComponent as C4 }  from './../assets/images/svg/icon_c4_default.svg';
-import { ReactComponent as Defuse }  from './../assets/images/svg/icon_defuse_default.svg';
-import { ReactComponent as Health }  from './../assets/images/svg/icon_health_default.svg';
-import { ReactComponent as HealthFull }  from './../assets/images/svg/icon_health_full_default.svg';
-import { ReactComponent as Hourglass }  from './../assets/images/svg/icon_hourglass_default.svg';
-import { ReactComponent as Microphone }  from './../assets/images/svg/icon_microphone.svg';
-import { ReactComponent as Pause }  from './../assets/images/svg/icon_pause_default.svg';
-import { ReactComponent as Skull }  from './../assets/images/svg/icon_skull_default.svg';
-import { ReactComponent as Timer }  from './../assets/images/svg/icon_timer_default.svg';
+import { ReactComponent as ArmorFull } from './../assets/images/icon_armor_full_default.svg';
+import { ReactComponent as ArmorHalf } from './../assets/images/icon_armor_half_default.svg';
+import { ReactComponent as ArmorHalfHelmet } from './../assets/images/icon_armor_half_helmet_default.svg';
+import { ReactComponent as ArmorHelmet } from './../assets/images/icon_armor_helmet_default.svg';
+import { ReactComponent as ArmorNone } from './../assets/images/icon_armor_none_default.svg';
+import { ReactComponent as Blind } from './../assets/images/icon_blind.svg';
+import { ReactComponent as Bomb } from './../assets/images/icon_bomb_default.svg';
+import { ReactComponent as BombExplosion }  from './../assets/images/icon_bomb_explosion_default.svg';
+import { ReactComponent as Bullets }  from './../assets/images/icon_bullets_default.svg';
+import { ReactComponent as Burning }  from './../assets/images/icon_burning.svg';
+import { ReactComponent as C4 }  from './../assets/images/icon_c4_default.svg';
+import { ReactComponent as Defuse }  from './../assets/images/icon_defuse_default.svg';
+import { ReactComponent as Health }  from './../assets/images/icon_health_default.svg';
+import { ReactComponent as HealthFull }  from './../assets/images/icon_health_full_default.svg';
+import { ReactComponent as Hourglass }  from './../assets/images/icon_hourglass_default.svg';
+import { ReactComponent as Microphone }  from './../assets/images/icon_microphone.svg';
+import { ReactComponent as Pause }  from './../assets/images/icon_pause_default.svg';
+import { ReactComponent as Skull }  from './../assets/images/icon_skull_default.svg';
+import { ReactComponent as Timer }  from './../assets/images/icon_timer_default.svg';
 import LogoCT  from './../assets/images/logo_CT_default.png';
 import LogoT  from './../assets/images/logo_T_default.png';
-import { ReactComponent as SmallBomb }  from "./../assets/images/svg/bomb.svg";
+import { ReactComponent as SmallBomb }  from "./../assets/images/bomb.svg";
 
 
 export {
