17584f1dca7f0132b25ea72a469f51a91ba82007
Hubert Walczak <hubertwalczak8@gmail.com>
2021-01-08 09:18:02 +0100

    Added keybind

--- src/HUD/Players/Avatar.tsx
+++ src/HUD/Players/Avatar.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
 import { isDev, port } from '../../api/api';
+import { actions } from '../../App';
 
 import { avatars } from './../../api/avatars';
 
 import { Skull } from './../../assets/Icons';
 
-export default class Avatar extends React.Component<{steamid: string, slot?: number, height?: number, width?: number, showSkull?: boolean, showCam?: boolean}> {
+interface IProps {
+  steamid: string,
+  slot?: number,
+  height?: number,
+  width?: number,
+  showSkull?: boolean,
+  showCam?: boolean
+}
+interface IState {
+  enableCams: boolean
+}
+export default class Avatar extends React.Component<IProps, IState> {
+  state = {
+    enableCams: !!this.props.showCam
+  }
+  componentDidMount(){
+    actions.on("toggleCams", () => {
+      this.setState({enableCams: !this.state.enableCams})
+  });
+  }
   render(){
+    const { enableCams } = this.state;
     const url = avatars.filter(avatar => avatar.steamid === this.props.steamid)[0];
     if(!url || (!url.steam.length && !url.custom.length)){
         return '';
@@ -17,7 +38,7 @@ export default class Avatar extends React.Component<{steamid: string, slot?: num
     return (
       <div className={`avatar`}>
           {
-            this.props.showCam ? <div  id="cameraFeed"><iframe style={{top: `${topPosition}px`, left: `${leftPosition}px`}} src={isDev ? `http://localhost:${port}/rmtp.html` : '/rmtp.html'} title="Camera feed" /></div> : null
+            this.props.showCam ? <div  id="cameraFeed" style={{ display: enableCams ? 'block' : 'none'}}><iframe style={{top: `${topPosition}px`, left: `${leftPosition}px`}} src={isDev ? `http://localhost:${port}/rmtp.html` : '/rmtp.html'} title="Camera feed" /></div> : null
           }
           <img src={this.props.showSkull ? Skull : (url.custom || url.steam)} height={this.props.height} width={this.props.width} alt={'Avatar'} />
       </div>
