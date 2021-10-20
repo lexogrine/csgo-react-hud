85ed8aa4386419c55ec4e816f706ecd8b564cacc
Hubert Walczak <hubertwalczak8@gmail.com>
2021-10-20 16:53:52 +0200

    Improved stability of webcam

--- src/HUD/Camera/index.tsx
+++ src/HUD/Camera/index.tsx
@@ -1,15 +1,16 @@
 import React, { useEffect } from "react";
+import { Instance, SignalData } from 'simple-peer';
+const Peer = require('simple-peer');
 
 
 //let peerConnection: RTCPeerConnection | null = null;
 type CameraConnectionInfo = {
-    peerConnection: RTCPeerConnection | null,
+    peerConnection: Instance | null,
     steamid: string
 }
 
 type OfferData = {
-    offer: RTCSessionDescriptionInit,
-    step: number;
+    offer: SignalData,
 }
 
 
@@ -19,59 +20,74 @@ const connectionsInfo: CameraConnectionInfo[] = []
 const PlayerCamera = (props: { steamid: string, roomId: string, socket: SocketIOClient.Socket, visible: boolean }) => {
     const getConnectionInfo = (steamid: string) => connectionsInfo.find(connection => connection.steamid === steamid) || null;
     const closeConnection = () => {
-        const remoteVideo = document.getElementById(`remote-video-${props.steamid}`);
+        try {
+            const remoteVideo = document.getElementById(`remote-video-${props.steamid}`);
+    
+            //connectionInfo.steamid = '';
+            const connectionInfo = getConnectionInfo(props.steamid);
+    
+            if (remoteVideo) (remoteVideo as any).srcObject = null;
+            if (!connectionInfo || !connectionInfo.peerConnection) {
+                // initiateVideoTransmission();
 
-        //connectionInfo.steamid = '';
-        const connectionInfo = getConnectionInfo(props.steamid);
-
-        if(remoteVideo) (remoteVideo as any).srcObject = null;
-        if(!connectionInfo || !connectionInfo.peerConnection) {
-            initiateVideoTransmission();
+                setTimeout(() => {
+                    initiateVideoTransmission();
+                }, 500)
+                return;
+            }
+    
+            connectionInfo.peerConnection.destroy();
+    
+            connectionInfo.peerConnection = null;
             return;
+        } catch {
+
         }
 
-        connectionInfo.peerConnection.close();
-
-        if(connectionInfo.peerConnection.iceConnectionState === 'closed') {
-            connectionInfo.peerConnection = null;
-            initiateVideoTransmission();
-            return;
-        };
     }
 
 
     const initiateVideoTransmission = () => {
+        const { socket, roomId, steamid } = props;
+
         let connectionInfo = getConnectionInfo(props.steamid);
-        if(!connectionInfo){
+        if (!connectionInfo) {
             connectionInfo = {
                 steamid: props.steamid,
                 peerConnection: null
             }
             connectionsInfo.push(connectionInfo);
         }
-        const rtcPeerConnectionConfiguration = {
-            // Server for negotiating traversing NATs when establishing peer-to-peer communication sessions
-            iceServers: [{
-                urls: ['stun:stun.l.google.com:19302', 'stun:stun4.l.google.com:19302']
-            }]
-        };
-        const peerConnection = new RTCPeerConnection(rtcPeerConnectionConfiguration);
+        const peerConnection: Instance = new Peer({ initiator: false, trickle: false });
         connectionInfo.peerConnection = peerConnection;
 
-        peerConnection.ontrack = ({ streams: [stream] }) => {
-            const remoteVideo = document.getElementById(`remote-video-${props.steamid}`);
-            if(!remoteVideo) return;
+        peerConnection.on('signal', answer => {
+            const offer = JSON.parse(JSON.stringify(answer)) as RTCSessionDescriptionInit;
+            socket.emit("offerFromHUD", roomId, { offer }, steamid);
 
-            (remoteVideo as any).srcObject = stream;
-        }
-        peerConnection.oniceconnectionstatechange = (ev) => {
-            if(!peerConnection) return;
+        });
 
-            const state = peerConnection.iceConnectionState;
-            if(state === 'disconnected'){
-                closeConnection();
-            }
-        }
+        peerConnection.on('error', (err) => {
+            console.log(err)
+            closeConnection();
+        })
+
+        peerConnection.on('stream', stream => {
+            const remoteVideo = document.getElementById(`remote-video-${props.steamid}`) as HTMLVideoElement;
+
+            if (!remoteVideo) return;
+
+            remoteVideo.srcObject = stream;
+
+            remoteVideo.play()
+        });
+
+        peerConnection.on('close', () => {
+            if (!peerConnection) return;
+
+            closeConnection();
+        });
+
     }
 
     useEffect(() => {
@@ -79,23 +95,17 @@ const PlayerCamera = (props: { steamid: string, roomId: string, socket: SocketIO
         const { socket } = props;
         socket.on('offerFromPlayer', async (roomId: string, offerData: OfferData, steamid: string) => {
             const connectionInfo = getConnectionInfo(props.steamid);
-            if(!connectionInfo) return;
+            if (!connectionInfo) return;
             const { peerConnection } = connectionInfo;
-            if(props.steamid !== steamid || props.roomId !== roomId || !peerConnection) return;
+            if (props.steamid !== steamid || props.roomId !== roomId || !peerConnection) return;
             
-            await peerConnection.setRemoteDescription(new RTCSessionDescription(offerData.offer));
-            const answer = await peerConnection.createAnswer();
-            await peerConnection.setLocalDescription(new RTCSessionDescription(answer));
-            
-            const offer = JSON.parse(JSON.stringify(answer)) as RTCSessionDescriptionInit;
-
-            socket.emit("offerFromHUD", roomId, { offer, step: offerData.step+1 }, steamid);
+            peerConnection.signal(offerData.offer)
 
         });
     }, []);
 
     return <React.Fragment>
-        <video className="video-call-preview" autoPlay muted id={`remote-video-${props.steamid}`} style={{ opacity: props.visible ? 1 : 0.001}}></video>
+        <video className="video-call-preview" autoPlay muted id={`remote-video-${props.steamid}`} style={{ opacity: props.visible ? 1 : 0.001 }}></video>
     </React.Fragment>
 }
 
